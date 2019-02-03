7cc86c07b96b6d59283c2f78e9d1d3527ef6d372
LaVielle <samuelcmann@gmail.com>
2019-02-03 23:20:23 +0100

    style cart table, add totals section and calculation

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import '../styles/CartItem.css'
 
 const CartItem = (props) => (
-  <div>{props.product.name}</div>
+  <div className="CartItem-container">
+    <div style={{flex: 4}} className="CartItem-name">{props.product.name}</div>
+    <div style={{flex: 2}} className="CartItem-count">{props.count}</div>
+    <div style={{flex: 2}} className="CartItem-unitPrice">{props.product.price}</div>
+    <div style={{flex: 3}} className="CartItem-total">{props.count * props.product.price}</div>
+  </div>
 )
 
 export default CartItem
--- client/src/components/CartTable.js
+++ client/src/components/CartTable.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import CartItem from './CartItem';
+import '../styles/CartTable.css';
 
 const CartTable = ({ products }) => {
   console.log(products);
+
+  const subtotal = products.reduce((subtotal, item) => subtotal + item.count * item.product.price, 0)
+  const numberOfUnits = products.reduce((numberOfUnits, item) => numberOfUnits + item.count, 0)
+  const shippingCostPerUnit = 5
+  const totalShipping = numberOfUnits * shippingCostPerUnit
+  const taxPercentage = .19
+  const taxTotal = (subtotal + totalShipping) * taxPercentage
+  const grandTotal = subtotal + totalShipping + taxTotal
+
   return (
-    <div>
-      {products.map(item => <CartItem key={item.product.id} {...item} />)}
+    <div className="CartTable-container">
+
+      {/*
+        Headers
+      */}
+      <div className="CartTable-headers">
+        <div className="CartTable-header-name">Product</div>
+        <div className="CartTable-header-count">Count</div>
+        <div className="CartTable-header-unitPrice">Unit Price</div>
+        <div className="CartTable-header-total">Total</div>
+      </div>
+
+      {/*
+        Items
+      */}
+      <div>
+        {products.map(item => <CartItem key={item.product.id} {...item} />)}
+      </div>
+
+
+      {/*
+        Totals
+      */}
+      <div className="CartTable-total-section">
+        <div className="CartTable-totals">
+          <div>Subtotal</div>
+          <div style={{ width: 20}} />
+          <div>{subtotal}</div>
+        </div>
+        <div className="CartTable-totals">
+          <div>Shipping</div>
+          <div style={{ width: 20}} />
+          <div>{totalShipping}</div>
+        </div>
+        <div className="CartTable-totals">
+          <div>VAT</div>
+          <div style={{ width: 20}} />
+          <div>{taxTotal}</div>
+        </div>
+        <div className="CartTable-totals">
+          <div>Total</div>
+          <div style={{ width: 20}} />
+          <div>{grandTotal}</div>
+        </div>
+      </div>
+
     </div>
   )
 }
