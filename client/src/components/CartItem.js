245a11a6ca3eff1148195b153f8963c31f375e4a
LaVielle <samuelcmann@gmail.com>
2019-02-04 10:29:33 +0100

    add plus and minus buttons to CartItem, inner logic not implemented yet

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -3,10 +3,24 @@ import '../styles/CartItem.css'
 
 const CartItem = (props) => (
   <div className="CartItem-container">
-    <div style={{flex: 4}} className="CartItem-name">{props.product.name}</div>
-    <div style={{flex: 2}} className="CartItem-count">{props.count}</div>
-    <div style={{flex: 2}} className="CartItem-unitPrice">{props.product.price}</div>
-    <div style={{flex: 3}} className="CartItem-total">{props.count * props.product.price}</div>
+    <div className="CartItem-name">{props.product.name}</div>
+    <div className="CartItem-count">
+      <div
+        className="CartItem-count-button"
+        onClick={() => {console.log('add item')}}
+      >
+        +
+      </div>
+      <div className="CartItem-count-counter">{props.count}</div>
+      <div
+        className="CartItem-count-button"
+        onClick={() => {console.log('remove item')}}
+      >
+        -
+      </div>
+    </div>
+    <div className="CartItem-unitPrice">{props.product.price}</div>
+    <div className="CartItem-total">{props.count * props.product.price}</div>
   </div>
 )
 
